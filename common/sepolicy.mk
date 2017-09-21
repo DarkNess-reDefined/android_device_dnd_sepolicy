db7487e1aafc760cb515f31c82645d2fcfb32e6b
DarkKnight6499 <yazad.madan@gmail.com>
2017-09-21 14:33:49 +0000

    DND-ify

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,7 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from DarkNess-reDefined
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common
+    device/dnd/sepolicy/common
